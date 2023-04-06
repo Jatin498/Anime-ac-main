888969509883cd13d8c138ea3ed92453924c972c
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-04-06 14:23:30 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -6,24 +6,26 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     HELP = """
-<b> ❏ Perintah untuk Pengguna BOT
- ├ /start - Mulai Bot
- ├ /about - Tentang Bot ini
- ├ /help - Bantuan Perintah Bot ini
- ├ /ping - Untuk mengecek bot hidup
- └ /uptime - Untuk melihat status bot 
- 
- ❏ Perintah Untuk Admin BOT
- ├ /logs - Untuk melihat logs bot
- ├ /setvar - Untuk mengatur var dengan command dibot
- ├ /delvar - Untuk menghapus var dengan command dibot
- ├ /getvar - Untuk melihat salah satu var dengan command dibot
- ├ /users - Untuk melihat statistik pengguna bot
- ├ /batch - Untuk membuat link lebih dari satu file
- ├ /speedtest - Untuk Mengetes kecepatan server bot
- └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
+<b>How to Use this Bot
 
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
+  ❏ Commands for BOT Users
+  ├ /start - Starts the Bot
+  ├ /about - About this Bot
+  ├ /help - Help this Bot Command
+  ├ /ping - To check live bots
+  └ /uptime - To see bot status
+
+  ❏ Commands For BOT Admins
+  ├ /logs - To view bot logs
+  ├ /setvar - To set var with dibot command
+  ├ /delvar - To remove var with dibot command
+  ├ /getvar - To see one of the var with dibot command
+  ├ /users - To view bot user statistics
+  ├ /batch - To link more than one file
+  ├ /speedtest - To test the bot server speed
+  └ /broadcast - To send a broadcast message to the bot user
+  
+ 👨‍💻 Developed by </b><a href='https://t.me/Campus_Bots_Update'>Anime Campus</a>
 """
 
     close = [
